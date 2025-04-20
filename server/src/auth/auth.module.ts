a2f52464c2161aa8bc49d66cdaff6cd889abbfd8
brandon <brandonchoi2016@gmail.com>
2025-04-20 14:16:56 -0700

    extend jwt to 1 day

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './passport/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
